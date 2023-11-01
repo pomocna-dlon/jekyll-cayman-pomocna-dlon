0270ab22b3d34c3d4a3e6d39a6e36e7719ddb114
Marek <jasonspine@gmail.com>
2023-11-01 04:44:23 +0100

    Hide unloaded images container

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
 			var container = this.closest('.archiveItem');
 			var images = container.querySelectorAll('img[data-src]');
-
+			var hiddenImagesContainer = container.querySelector('.centerImgsEmpty');
+			
+			hiddenImagesContainer.classList.add('centerImgs');
+			hiddenImagesContainer.classList.remove('centerImgsEmpty');
+			
 			images.forEach(function(image) {
 				image.setAttribute('src', image.getAttribute('data-src'));
 				image.removeAttribute('data-src');
 			});
+			
+			this.remove();
 		});
 	});
 });
