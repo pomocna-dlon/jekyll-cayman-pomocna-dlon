9045b27ca97451875e9c74ef37b918ecb91bc3d9
Marek <jasonspine@gmail.com>
2023-11-04 03:45:32 +0100

    Gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ function openImageBrowser(event, index) {
 	event.preventDefault();
 	
 	const overlay = document.getElementById('overlay');
-	const image = document.getElementById('image');
 
 	images = JSON.parse(event.target.getAttribute('data-src'));
 	currentIndex = index;
@@ -16,23 +15,30 @@ function openImageBrowser(event, index) {
 
 function closeImageBrowser(event) {
 	event.preventDefault();
+	
 	const overlay = document.getElementById('overlay');
+	
 	overlay.style.display = 'none';
 }
 
 function displayImage(index) {
 	const image = document.getElementById('image');
+	const counter = document.getElementById('counter');
+	
 	image.src = images[index];
+	counter.textContent = index + '/' + images.length;
 }
 
 function navigateImage(event, offset) {
 	event.preventDefault();
 	
 	currentIndex += offset;
+	
 	if (currentIndex < 0) {
 		currentIndex = images.length - 1;
 	} else if (currentIndex >= images.length) {
 		currentIndex = 0;
 	}
+	
 	displayImage(currentIndex);
 }
